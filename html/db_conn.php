e533a47367b49f030faab58638319fd0c7ef070f
obafema <obafema@gmail.com>
2022-07-31 10:01:58 +0100

    reverting changes to db_conn.php

--- html/db_conn.php
+++ html/db_conn.php
@@ -10,7 +10,7 @@ $dotenv->load();
 // enabling environment variable for php
 
 
-$servername = "127.0.0.1"; // input servername
+$servername = "mysqlserverhost"; // input servername
 $username = "userconnect"; // input username
 $password = "zigali"; //input password
 $dbname = "toolingdb"; // input dbname
